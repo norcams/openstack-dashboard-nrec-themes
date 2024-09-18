d3199ab0f2d4a7560f027fea23b93401a3f2a03f
Ivar Thokle Hovden <ivarth@uio.no>
2024-09-18 10:15:45 +0200

    add local_settings.d for setting NREC themes

---
+++ local_settings.d/_10_set_nrec_themes.py
@@ -0,0 +1,8 @@
+# (c) Copyright 2024 NREC
+
+AVAILABLE_THEMES = [
+    ('default', 'Light', 'themes/default')
+    ('darkblue', 'Dark blue', 'themes/darkblue')
+]
+SITE_BRANDING = 'NREC'
+DEFAULT_THEME = 'default'
