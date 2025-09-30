15f678703f168f3af696609e5c43be3c5fb3d237
Ivar Thokle Hovden <ivarth@uio.no>
2025-09-30 10:55:48 +0200

    feat: add back Material theme

--- local_settings.d/_10_set_nrec_themes.py
+++ local_settings.d/_10_set_nrec_themes.py
@@ -1,6 +1,7 @@
 AVAILABLE_THEMES = [
     ('default', 'Default', 'themes/default'),
-    ('darkblue', 'Dark Blue', 'themes/darkblue')
+    ('darkblue', 'Dark Blue', 'themes/darkblue'),
+    ('material', 'Material', 'themes/material')
 ]
 SITE_BRANDING = 'NREC'
 DEFAULT_THEME = 'default'
